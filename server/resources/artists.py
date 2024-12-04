0cfb4e9a92b572c2f9a7611dc1e6a21ba51093c6
oyavri <oyavri@gmail.com>
2024-12-04 22:32:51 +0300

    Fix modify_artist bug, make SQL statements multi-line, change structure of returning errors

--- server/resources/artists.py
+++ server/resources/artists.py
@@ -10,20 +10,51 @@ INTERNAL_SERVER_ERROR = 500
 artists_bp = Blueprint('artists', __name__)
 db = db()
 
+def id_error():
+    return jsonify(
+            {
+                "error": "Artist id must be an integer"
+            }
+        ), BAD_REQUEST
+
+def internal_error():
+    return jsonify(
+                {
+                    "error": "An internal error occurred."
+                }
+            ), INTERNAL_SERVER_ERROR
+
+def no_artist():
+    return jsonify(
+                    {
+                        "error": "There is no such artist with given id."
+                    }
+                ), NOT_FOUND
+
+def no_data():
+    return jsonify(
+            {
+                "error": "Unsopported format of request"
+            }
+        ), BAD_REQUEST
+
+
 @artists_bp.route('/artists', methods=['GET'])
 def get_artists():
     try:
         connection = db.connect()
         cursor = connection.cursor(dictionary=True)
 
-        cursor.execute("SELECT * FROM ARTIST")
+        cursor.execute(f'''
+                       SELECT * FROM ARTIST;
+                       ''')
         results = cursor.fetchall()
 
         cursor.close()
         connection.close()
         return jsonify(results), OK
     except:
-        return jsonify({"error": "An internal error occurred."}), INTERNAL_SERVER_ERROR
+        return internal_error()
 
 @artists_bp.route('/artists/<artist_id>', methods=['GET'])
 def get_artist(artist_id):
@@ -33,21 +64,24 @@ def get_artist(artist_id):
         connection = db.connect()
         cursor = connection.cursor(dictionary=True)
 
-        cursor.execute(f'SELECT * FROM ARTIST WHERE id = {artist_id}')
+        cursor.execute(f'''
+                       SELECT * FROM ARTIST 
+                       WHERE id = {artist_id};
+                       ''')
         result = cursor.fetchone()
 
         if result is None:
             cursor.close()
             connection.close()
-            return jsonify({"error": "Artist not found."}), NOT_FOUND
+            return no_artist()
         
         cursor.close()
         connection.close()
         return jsonify(result), OK
     except ValueError:
-        return jsonify({"error": "Artist id must be an integer"}), BAD_REQUEST
+        return id_error()
     except:
-        return jsonify({"error": "An internal error occurred."}), INTERNAL_SERVER_ERROR
+        return internal_error()
 
 @artists_bp.route('/artists', methods=['POST'])
 def add_artist():
@@ -55,7 +89,7 @@ def add_artist():
         data = request.get_json()
 
         if not data:
-            return jsonify({"error": "Unsopported format of request"}), BAD_REQUEST
+            return no_data()
 
         name = data.get('name')
         short_info = data.get('short_info')
@@ -63,20 +97,32 @@ def add_artist():
         if not name:
             return jsonify({"error": "\"name\" field of the artist must be provided."}), BAD_REQUEST
         if not short_info:
-            short_info = ""
+            return jsonify({"error": "\"short_info\" field of the artist must be provided."}), BAD_REQUEST
+
         
         connection = db.connect()
         cursor = connection.cursor()
         
-        cursor.execute(f'INSERT INTO ARTIST (name, short_info) VALUES ("{name}", "{short_info}")')
+        cursor.execute('''
+                       INSERT INTO ARTIST (name, short_info) 
+                       VALUES (%s, %s)
+                       ''', (name, short_info))
         connection.commit()
 
         cursor.close()
         connection.close()
         
-        return jsonify({"message": "Artist added successfully.", "name": name, "short_info": short_info}), CREATED
+        return jsonify(
+            {
+                "message": "Artist added successfully.", 
+                "artist": {
+                    "id": cursor.lastrowid,
+                    "name": name,
+                    "short_info": short_info
+                }
+            }), CREATED
     except:
-        return jsonify({"error": "An internal error occurred."}), INTERNAL_SERVER_ERROR
+        return internal_error()
     
 @artists_bp.route('/artists/<artist_id>', methods=['PUT'])
 def update_artist(artist_id):
@@ -85,35 +131,52 @@ def update_artist(artist_id):
 
         data = request.get_json()
         if not data:
-            return jsonify({"error": "Unsopported format of request"}), BAD_REQUEST
+            return no_data()
 
         name = data.get('name')
         short_info = data.get('short_info')
 
         if not name:
-            return jsonify({"error": "Name of the artist cannot be null"}), BAD_REQUEST
+            return jsonify({"error": "\"name\" field of the artist must be provided."}), BAD_REQUEST
+        if not short_info:
+            return jsonify({"error": "\"short_info\" field of the artist must be provided."}), BAD_REQUEST
 
         connection = db.connect()
         cursor = connection.cursor()
 
-        cursor.execute(f'SELECT * FROM ARTIST WHERE id={artist_id}')
+        cursor.execute(f'''
+                       SELECT * FROM ARTIST 
+                       WHERE id={artist_id}
+                       ''')
         results = cursor.fetchone()
 
         if results is None:
             cursor.close()
             connection.close()
-            return jsonify({"error": "Artist not found."}), NOT_FOUND
+            return no_artist()
 
-        cursor.execute(f'UPDATE ARTIST SET name = "{name}", short_info = "{short_info}" WHERE id = {artist_id}')
+        cursor.execute(f'''
+                       UPDATE ARTIST 
+                       SET name = "{name}", short_info = "{short_info}" 
+                       WHERE id = {artist_id}
+                       ''')
         connection.commit()
 
         cursor.close()
         connection.close()
-        return jsonify({"message": "Artist updated successfully.", "name": name, "short_info": short_info}), OK
+        return jsonify(
+            {
+                "message": "Artist updated successfully.", 
+                "artist": {
+                    "id": artist_id,
+                    "name": name, 
+                    "short_info": short_info
+                }
+            }), OK
     except ValueError:
-        return jsonify({"error": "Artist id must be an integer"}), BAD_REQUEST
+        return id_error()
     except:
-        return jsonify({"error": "An internal error occurred."}), INTERNAL_SERVER_ERROR
+        return internal_error()
 
 @artists_bp.route('/artists/<artist_id>', methods=['PATCH'])
 def modify_artist(artist_id):
@@ -122,7 +185,7 @@ def modify_artist(artist_id):
 
         data = request.get_json()
         if not data:
-            return jsonify({"error": "Unsopported format of request"}), BAD_REQUEST
+            return no_data()
 
         name = data.get('name')
         short_info = data.get('short_info')
@@ -130,32 +193,51 @@ def modify_artist(artist_id):
         connection = db.connect()
         cursor = connection.cursor()
 
-        cursor.execute(f'SELECT * FROM ARTIST WHERE id={artist_id}')
+        cursor.execute(f'''
+                       SELECT * FROM ARTIST 
+                       WHERE id={artist_id}
+                       ''')
         result = cursor.fetchone()
 
         if result is None:
             cursor.close()
             connection.close()
-            return jsonify({"error": "Artist not found."}), NOT_FOUND
+            return no_artist()
         
-        # Update either one of them or both of them
-        if name:
-            cursor.execute(f'UPDATE ARTIST SET name = "{name}" WHERE id = {artist_id}')
-        elif short_info:
-            cursor.execute(f'UPDATE ARTIST SET short_info = {short_info} WHERE id = {artist_id}')
-        else:
-            cursor.execute(f'UPDATE ARTIST SET name = "{name}", short_info = "{short_info}" WHERE id = {artist_id}')
-
+        if not name and not short_info:
+            cursor.close()
+            connection.close()
+            return jsonify(
+                    {
+                        "error": "No modifiable field has been specified. Modifiable fields are: \"name\", \"short_info\"."
+                    }
+                ), BAD_REQUEST
+        
+        cursor.execute(f'''
+                        UPDATE ARTIST 
+                        SET {f"name = \"{name}\"," if name else ""}
+                            {f"short_info = \"{short_info}\"" if short_info else ""} 
+                        WHERE id = {artist_id}
+                        ''')
+    
         connection.commit()
 
         cursor.close()
         connection.close()
 
-        return jsonify({"message": "Artist updated successfully.", "name": name, "short_info": short_info}), OK
+        return jsonify(
+            {
+                "message": "Artist updated successfully.", 
+                "artist": {
+                    "id": artist_id,
+                    "name": name, 
+                    "short_info": short_info
+                }
+            }), OK
     except ValueError:
-        return jsonify({"error": "Artist id must be an integer"}), BAD_REQUEST
+        return id_error()
     except:
-        return jsonify({"error": "An internal error occurred."}), INTERNAL_SERVER_ERROR
+        return internal_error()
 
 @artists_bp.route('/artists/<artist_id>', methods=['DELETE'])
 def delete_artist(artist_id):
@@ -179,8 +261,12 @@ def delete_artist(artist_id):
         cursor.close()
         connection.close()
 
-        return jsonify({"message": "Artist deleted successfully."}), OK
+        return jsonify(
+                {
+                    "message": "Artist deleted successfully."
+                }
+            ), OK
     except ValueError:
-        return jsonify({"error": "Artist id must be an integer"}), BAD_REQUEST
+        return id_error()
     except:
-        return jsonify({"error": "An internal error occurred."}), INTERNAL_SERVER_ERROR
+        return internal_error()
