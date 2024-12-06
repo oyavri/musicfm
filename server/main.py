f1133fe2f55344f20bc1a138d9be968916aa7b05
oyavri <oyavri@gmail.com>
2024-12-06 15:12:17 +0300

    Add db initialization before flask app runs, remove likes.py remains

--- server/main.py
+++ server/main.py
@@ -6,7 +6,6 @@ from resources.artists import artists_bp
 from resources.albums import albums_bp
 from resources.tracks import tracks_bp
 from resources.users import users_bp
-from resources.likes import likes_bp
 
 app = Flask(__name__)
 app.logger.setLevel(logging.ERROR)
@@ -15,7 +14,6 @@ app.json.ensure_ascii = False # due to turkish characters
 
 artists_bp.register_blueprint(tracks_bp)
 artists_bp.register_blueprint(albums_bp)
-users_bp.register_blueprint(likes_bp)
 app.register_blueprint(artists_bp, url_prefix="/")
 app.register_blueprint(users_bp, url_prefix="/")
 
@@ -24,4 +22,5 @@ def hello():
     return "Hello world!"
 
 if __name__ == "__main__":
+    db() # init db
     app.run(host="0.0.0.0", port=5000, debug=True)
