e596edab323d19c7751a8c4a3ac4cd7d1ad8e99b
oyavri <oyavri@gmail.com>
2024-12-06 15:38:58 +0300

    Implement GET likes, GET rates to users endpoint

--- server/resources/users.py
+++ server/resources/users.py
@@ -28,6 +28,13 @@ def id_error():
         }
     ), BAD_REQUEST
 
+def id_error_including_track():
+    return jsonify(
+        {
+            "error": "User ID and track ID must be an integer."
+        }
+    ), BAD_REQUEST
+
 def internal_error():
     return jsonify(
         {
@@ -39,6 +46,13 @@ def no_user():
     return jsonify(
         {
             "error": "There is no such user with given ID."
+        }
+    ), NOT_FOUND
+
+def no_track():
+    return jsonify(
+        {
+            "error": "There is no track associated with given album."
         }
     ), NOT_FOUND
 
@@ -384,5 +398,212 @@ def delete_user(user_id):
 
     except ValueError:
         return id_error()
+    except:
+        return internal_error()
+
+# LIKE PART
+
+@users_bp.route('/users/<user_id>/likes', methods=['GET'])
+def get_likes_of_user(user_id):
+    try:
+        user_id = int(user_id)
+
+        connection = db.connect()
+        cursor = connection.cursor(dictionary=True)
+        
+        cursor.execute(f'''
+                       SELECT * FROM USER 
+                       WHERE id = {user_id};
+                       ''')
+        user = cursor.fetchone()
+
+        if user is None:
+            cursor.close()
+            connection.close()
+            return no_user()
+        
+        cursor.execute(f'''SELECT track.id, track.name, track.length_sec, track.album_id FROM USER AS user 
+                       JOIN USER_LIKE as user_like
+                       ON user_like.user_id = user.id
+                       JOIN TRACK as track
+                       ON user_like.track_id = track.id
+                       WHERE user.id = {user_id};
+                       ''')
+        likedTracks = cursor.fetchall()
+
+        if not likedTracks:
+            cursor.close()
+            connection.close()
+            return jsonify(
+                {
+                    "message": "The user has not liked any track yet."
+                }
+            ), OK
+        
+        cursor.close()
+        connection.close()
+
+        return jsonify(likedTracks), OK
+        
+    except ValueError:
+        return id_error()
+    except:
+        return internal_error()
+
+
+@users_bp.route('/users/<user_id>/likes/<track_id>', methods=['GET'])
+def get_like_of_user(user_id, track_id):
+    try:
+        user_id = int(user_id)
+        track_id = int(track_id)
+
+        connection = db.connect()
+        cursor = connection.cursor(dictionary=True)
+        
+        cursor.execute(f'''
+                       SELECT * FROM USER 
+                       WHERE id = {user_id};
+                       ''')
+        user = cursor.fetchone()
+
+        if user is None:
+            cursor.close()
+            connection.close()
+            return no_user()
+        
+        cursor.execute(f'''
+                       SELECT * FROM TRACK 
+                       WHERE id = {track_id};
+                       ''')
+        track = cursor.fetchone()
+
+        if track is None:
+            cursor.close()
+            connection.close()
+            return no_track()
+        
+        cursor.execute(f'''SELECT * FROM USER_LIKE
+                       WHERE user_id = {user_id} AND track_id = {track_id};
+                       ''')
+        like = cursor.fetchone()
+
+        response = { "liked": f"{ like is not None }" }
+        
+        cursor.close()
+        connection.close()
+
+        return jsonify(response), OK
+        
+    except ValueError:
+        return id_error_including_track()
+    except:
+        return internal_error()
+
+
+# RATE PART
+
+@users_bp.route('/users/<user_id>/rates', methods=['GET'])
+def get_rates_of_user(user_id):
+    try:
+        user_id = int(user_id)
+
+        connection = db.connect()
+        cursor = connection.cursor(dictionary=True)
+        
+        cursor.execute(f'''
+                       SELECT * FROM USER 
+                       WHERE id = {user_id};
+                       ''')
+        user = cursor.fetchone()
+
+        if user is None:
+            cursor.close()
+            connection.close()
+            return no_user()
+        
+        cursor.execute(f'''SELECT track.id, track.name, track.length_sec, track.album_id, rate.rate FROM USER AS user 
+                       JOIN RATE AS rate
+                       ON rate.user_id = user.id
+                       JOIN TRACK AS track
+                       ON rate.track_id = track.id
+                       WHERE user.id = {user_id};
+                       ''')
+        ratedTracks = cursor.fetchall()
+
+        if not ratedTracks:
+            cursor.close()
+            connection.close()
+            return jsonify(
+                {
+                    "message": "The user has not rated any track yet."
+                }
+            ), OK
+        
+        cursor.close()
+        connection.close()
+
+        return jsonify(ratedTracks), OK
+        
+    except ValueError:
+        return id_error()
+    except:
+        return internal_error()
+
+@users_bp.route('/users/<user_id>/rates/<track_id>', methods=['GET'])
+def get_rate_of_user(user_id, track_id):
+    try:
+        user_id = int(user_id)
+        track_id = int(track_id)
+
+        connection = db.connect()
+        cursor = connection.cursor(dictionary=True)
+        
+        cursor.execute(f'''
+                       SELECT * FROM USER 
+                       WHERE id = {user_id};
+                       ''')
+        user = cursor.fetchone()
+
+        if user is None:
+            cursor.close()
+            connection.close()
+            return no_user()
+        
+        cursor.execute(f'''
+                       SELECT * FROM TRACK 
+                       WHERE id = {track_id};
+                       ''')
+        track = cursor.fetchone()
+
+        if track is None:
+            cursor.close()
+            connection.close()
+            return no_track()
+        
+        cursor.execute(f'''SELECT track.id, track.name, track.length_sec, track.album_id, rate.rate FROM USER AS user 
+                       JOIN RATE AS rate
+                       ON rate.user_id = user.id
+                       JOIN TRACK AS track
+                       ON rate.track_id = track.id
+                       WHERE user.id = {user_id};
+                       ''')
+        ratedTrack = cursor.fetchone()
+
+        if ratedTrack is None:
+            cursor.close()
+            connection.close()
+            return jsonify(
+                {
+                    "message": "The user has not rated this track yet."
+                }
+            ), OK
+        
+        cursor.close()
+        connection.close()
+
+        return jsonify(ratedTrack), OK
+        
+    except ValueError:
+        return id_error_including_track()
     except:
         return internal_error()
